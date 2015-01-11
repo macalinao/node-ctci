dabfa3c56cb0dd0476a8c63b3fea5306202d4b7a
Ian Macalinao <me@ian.pw>
2015-01-10 20:52:15 -0600

    Add 2.1 without a buffer

--- problems/2.1.js
+++ problems/2.1.js
@@ -23,3 +23,28 @@ module.exports = function removeDups(list) {
   }
   return list;
 };
+
+module.exports.noBuffer = function removeDups(list) {
+  // i love js
+  list.containsBeforeIndex = function(val, index) {
+    var it = this.iterator();
+    for (var i = 0; i < index; i++) {
+      if (it.next() == val) return true;
+    }
+    return false;
+  };
+
+  var last = list.head;
+  var node = last.next;
+  for (var i = 1; node;) {
+    if (list.containsBeforeIndex(node.value, i)) {
+      last.next = node.next;
+      node = node.next;
+    } else {
+      last = node;
+      node = node.next;
+      i++;
+    }
+  }
+  return list;
+};
--- problems/2.1.spec.js
+++ problems/2.1.spec.js
@@ -8,4 +8,9 @@ describe('2.1', function() {
     var expected = [4, 2, 5, 8, 13, 17, 9, 0];
     expect(removeDups(list).toArray()).to.eql(expected);
   });
+  it('should remove duplicates no buffer', function() {
+    var list = new LinkedList([4, 2, 5, 5, 8, 13, 17, 13, 9, 0, 2]);
+    var expected = [4, 2, 5, 8, 13, 17, 9, 0];
+    expect(removeDups.noBuffer(list).toArray()).to.eql(expected);
+  });
 });
