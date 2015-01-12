ea4f56bca037e954c77c6c34b43f218f2565f73d
Ian Macalinao <me@ian.pw>
2015-01-12 02:50:19 -0600

    Shuffle to prove correctness

--- problems/3.1.spec.js
+++ problems/3.1.spec.js
@@ -6,20 +6,18 @@ describe('3.1', function() {
   it('should implement 3 stacks', function() {
     var stacks = ThreeStacks();
 
+    stacks.push(5, 1);
     stacks.push(4, 0);
     stacks.push(3, 0);
-    stacks.push(2, 0);
-    stacks.push(1, 0);
-
-    stacks.push(5, 1);
-    stacks.push(4, 1);
-    stacks.push(3, 1);
-    stacks.push(2, 1);
-
     stacks.push(1, 2);
     stacks.push(2, 2);
+    stacks.push(4, 1);
+    stacks.push(3, 1);
     stacks.push(3, 2);
     stacks.push(4, 2);
+    stacks.push(2, 0);
+    stacks.push(1, 0);
+    stacks.push(2, 1);
 
     expect(stacks.pop(0)).to.equal(1);
     expect(stacks.pop(0)).to.equal(2);
