07cf485fb5f3bc0684470db30e02f1181189fd9b
Ian Macalinao <me@ian.pw>
2015-01-12 15:40:57 -0600

    Better solution to 4.1

--- problems/4.1.js
+++ problems/4.1.js
@@ -1,21 +1,11 @@
 function minDepth(node) {
   if (!node) return 0;
-  if (node.left && node.right) {
-    return Math.min(minDepth(node.left), minDepth(node.right)) + 1;
-  } else {
-    return 1;
-  }
+  return Math.min(minDepth(node.left), minDepth(node.right)) + 1;
 }
 
 function maxDepth(node) {
   if (!node) return 0;
-  if (node.left && node.right) {
-    return Math.max(maxDepth(node.left), maxDepth(node.right)) + 1;
-  } else if (node.left) {
-    return maxDepth(node.left) + 1;
-  } else {
-    return maxDepth(node.right) + 1;
-  }
+  return Math.max(maxDepth(node.left), maxDepth(node.right)) + 1;
 }
 
 module.exports = function isBalanced(node) {
